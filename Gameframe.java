7471216275b51295c80c61238eb7a2b75a5064f3
hayakahiji <hayakahiji@gmail.com>
2026-02-14 23:19:52 +0700

    add V2 Gameframe

--- Gameframe.java
+++ Gameframe.java
@@ -2,13 +2,28 @@ import java.awt.FlowLayout;
 
 import javax.swing.*;
 
-public class Gameframe extends JFrame{
-        public Gameframe() {
-            add(new GamePanel());
-            setTitle("Game");
-            setSize(1000,600);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setVisible(true);
-            setResizable(false);
+public class Gameframe extends JFrame {
+
+
+    public Gameframe() {
+        add(new GamePanel());
+        setTitle("Game");
+        setSize(1000, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true); // ถ้าไม่มี frame ไม่ขึ้น
+        setLocationRelativeTo(null);
+        setResizable(false);
+        showMenu();
     }
+
+    public void showMenu() { // จะเป็นหน้าจอมีปุ่ม Start
+        setContentPane(new MenuPanel(this));
+        revalidate();
+    }
+
+    public void startGame() {
+        setContentPane(new GamePanel());
+        revalidate();
+    }
+
 }
